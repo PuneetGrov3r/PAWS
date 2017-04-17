76f19eb9c45ca77b96483eef08b6108aba769025
Puneet Grover <grover.puneet1995@gmail.com>
2017-04-17 19:37:54 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,96 @@
+import nltk
+from nltk.corpus import state_union
+from nltk.tokenize import PunktSentenceTokenizer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words("english"))
+from nltk.stem.snowball import SnowballStemmer
+stemmer = SnowballStemmer("english")
+import json
+import requests
+from pprint import pprint
+
+lemmatizer = WordNetLemmatizer()
+ss= [",", "."]
+ques = {"default":{
+					"How may I help you?\n",
+					"What's going on ",
+					"You should try something new!\n",
+					"Get a life, for christ's sake."},
+        "Where":{
+        			(" do you want to go?", " to?", " would you like to go?"), # Travel based
+        			(" do you want to eat?", "Would you like something fancy or easy on pocket?", "?"), # For restraunt     {1st position does not need where}
+        			(" do you want "),
+        		},
+        "When":{
+        			(" do you want to go?", " do you want to leave?", " would you like to schedule your ", "At what time?","?"), #Travel based    {2nd "departure", "return"}
+        			(" do you want to eat?", " would you like it?", "?"), #Restraunt based
+        			("","")
+        },
+        "What":{
+        			(" would you like?"),
+        			(" se")
+        },
+        "How":{
+        			(" do you want to go?"),       # Default
+        			(" would you like to go?", "So, through air or by road?")     # Travel
+        },
+        "Which":{
+        			(" one would you like?"),   #Default
+        			(" ")
+        }
+        "":{}
+        }
+
+zomatoDefaultURL = "https://developers.zomato.com/api/v2.1/"
+zomatoHeader = {"User-agent": "curl/7.43.0", "Accept": "application/json", "user_key": "8c566e4798eca2737581bd3c21390711"}
+zomatoAPI = {"locations":"/locations?", "location_details":"/location_details?", "cuisines":"/cuisines?", "restraunt":"/restraunt?", "reviews":"/reviews?"}
+zomatoBack = {"query":"query=", "entity_id":"entity_id=", "entity_type":"entity_type=", "rest_id":"rest_id="}
+zomatoFore = {'city':0, 'time':0, 'rest':0, 'number':1}
+zomatoFormulate = {"query":"", "entity_id":"", "entity_type":"", "rest":""}
+
+
+def fetch_location(url):
+    zomatoFormulate["query"] = "delhi"
+    location = url + zomatoFormulate["query"]
+    return location
+
+def fetch_cuisine(obj):
+    cuisine  = 0
+    return cuisine
+
+def fetch_rest(obj):
+    rest = 0
+    return rest
+
+def fetch_number(obj):
+    number = 0
+    return number
+
+
+cont = True
+url = zomatoDefaultURL + zomatoAPI["locations"] + zomatoBack["query"]
+
+
+while(cont):
+    sent = input(ques["default"][0])
+    tagged = []
+    words = nltk.word_tokenize(sent)
+    filtered = [ lemmatizer.lemmatize(w) for w in words if (w not in stop_words and w not in ss) ]
+    tagged.append(nltk.pos_tag(filtered))
+    output = {'Noun':[],
+              'Verb':[]}
+    for i in tagged:
+        for j in i:
+            if j[1] in ['NN' , 'NNS' , 'NNP' , 'NNPS']:
+                output['Noun'].append(j[0])
+            elif j[1] in ['VB' , 'VBD' , 'VBG' , 'VBN' , 'VBP' , 'VBZ']:
+                output['Verb'].append(j[0])
+    print(json.dumps(output, indent=4))
+    url = fetch_location(url)
+    response = requests.get(url, headers=zomatoHeader)
+    pprint(response.json())
+
+    
+    cont = False
+# THE END
---
+++ project3.py
@@ -0,0 +1,22 @@
+from nltk.parse.stanford import StanfordNeuralDependencyParser
+from os.path import expanduser
+import os
+import json
+
+os.environ["JAVAHOME"] = 'C:/Program Files/Java/jdk1.8.0_121/bin/java.exe'
+os.environ['CLASSPATH'] = 'C:/Users/PuneetGrover/AppData/Roaming/nltk_data/stanford-corenlp/stanford-corenlp-3.7.0.jar'
+_path_to_models_jar = 'C:/Users/PuneetGrover/Downloads/stanfordNLP/stanford-english-corenlp-2016-10-31-models.jar'
+
+
+dep_parser = StanfordNeuralDependencyParser(path_to_models_jar=_path_to_models_jar, java_options='-mx4g')
+
+
+#print([parse.tree() for parse in dep_parser.raw_parse("The quick brown fox jumps over the lazy dog.")])
+print(json.dumps([list(parse.triples()) for parse in dep_parser.raw_parse("Bell, based in Los Angeles, makes and distributes electronic, computer and building products.")], indent=4))
+
+#sum([[parse.tree() for parse in dep_graphs] for dep_graphs in dep_parser.raw_parse_sents((
+#    "The quick brown fox jumps over the lazy dog.",
+#    "The quick grey wolf jumps over the lazy fox."))], [])
+#sum([[parse.tree() for parse in dep_graphs] for dep_graphs in dep_parser.parse_sents((
+#    "I 'm a dog".split(),
+#    "This is my friends ' cat ( the tabby )".split(),))], [])
