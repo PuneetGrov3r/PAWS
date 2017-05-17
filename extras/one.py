9d72b2829baea4f615fc0cd420762469365918ab
Puneet Grover <grover.puneet1995@gmail.com>
2017-05-17 20:45:16 +0530

    Add files via upload

--- extras/one.py
+++ extras/one.py
@@ -7,6 +7,56 @@ from zomatoAPI import *
 import time
 import sys
 import os
+from nltk.parse.stanford import StanfordParser
+from nltk.parse.stanford import StanfordDependencyParser
+
+
+def ifnv(string):
+    if string in ['NN' , 'NNS' , 'NNP' , 'NNPS']:
+        return 1
+    elif string in ['VB' , 'VBD' , 'VBG' , 'VBN' , 'VBP' , 'VBZ']:
+        return 2
+    else:
+        return 0
+
+def getNodes(parent, abc):
+    named_entity = []
+    ROOT = 'ROOT'
+    for node in parent:
+        if type(node) is nltk.Tree:
+            '''
+            if node.label() == ROOT:
+                print("======== Sentence =========")
+                print("Sentence:", " ".join(node.leaves()))
+            else:
+                print("Label:", node.label())
+                print("Leaves:", node.leaves())
+            '''
+            if node.label() == 'NP' :
+                named_entity.append(node.leaves())
+            if ifnv(node.label()) == 1:
+                abc['Noun'].append(node.leaves()[0])
+            elif ifnv(node.label()) == 2:
+                abc['Verb'].append(node.leaves()[0])
+            getNodes(node, abc)
+        #else:
+            #print("Word:", node)
+
+def stanfordparser(cwd, sent, parser):
+    abc = {'Noun':[],
+              'Verb':[],
+              'Other':[]}
+    tree = list(parser.raw_parse(sent))
+    #print(tree)
+    getNodes(tree, abc)
+    return abc
+    
+
+
+
+
+
+
 
 def psleep(n, t = 0.1):
     for i in range(n):
@@ -20,9 +70,9 @@ def filterword(tagged):
     print("\n\nFiltering the words...\n")
     for i in tagged:
         for j in i:
-            if j[1] in ['NN' , 'NNS' , 'NNP' , 'NNPS']:
+            if ifnv(j[1]) == 1:
                 output['Noun'].append(j[0])
-            elif j[1] in ['VB' , 'VBD' , 'VBG' , 'VBN' , 'VBP' , 'VBZ']:
+            elif ifnv(j[1]) == 2:
                 output['Verb'].append(j[0])
             else:
                 output['Other'].append(j[0])
@@ -54,10 +104,20 @@ print("  Tagger ready ...")
 psleep(21, 0.01)
 print("\n\n")
 
+
+
+os.environ['STANFORD_PARSER'] = cwd + '/stanford-parser'
+os.environ['CLASSPATH'] = cwd + '/stanford-parser/stanford-parser-3.7.0-models.jar'
+parser=StanfordParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
+
+
+
+
+
 cont = True
 while(cont):
     sent = input(ques["default"][0])
-
+    #output2 = stanfordparser(cwd, sent, parser)
     print("\n\nTokenizing the words...\n")
     words = nltk.word_tokenize(sent)
     print(words)
@@ -67,11 +127,25 @@ while(cont):
     tagged.append(st.tag(words))
     print(tagged)
     
-    output = filterword(tagged)
+    #output = filterword(tagged)
+    '''
+    for w in words:
+        if w not in output2['Noun']:
+            if w not in output2['Verb']:
+                output2['Other'].append(w) 
+    print(json.dumps(output2, indent=4))
+    '''
+
+
+    dep_parser = StanfordDependencyParser(model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
+    dependencyGraph = [list(parse.triples()) for parse in dep_parser.raw_parse(sent)][0]
+    
+    print(json.dumps(dependencyGraph, indent=4))
+
 
     dictnoun = {}
     print("\n\nFinding attributes of some nouns...\n")
-    dictnoun = findattr1(dictnoun, output['Noun'])
+    ##dictnoun = findattr1(dictnoun, output2['Noun'])
     print(json.dumps(dictnoun, indent = 4))
 
     cont = False
