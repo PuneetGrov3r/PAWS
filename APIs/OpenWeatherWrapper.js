61eac223d5f509d7614d3f87c5cd4129746ff010
PuneetGrov3r <grover.puneet1995@gmail.com>
2017-05-31 21:47:08 +0530

    0.2.3

--- APIs/OpenWeatherWrapper.js
+++ APIs/OpenWeatherWrapper.js
@@ -19,14 +19,17 @@ const Weather = () => ({
 					resolve(JSON.parse(body));
 				});
 				p.then( (data) => {
-					console.log(data)
-					if(data['sys']['counntry'] && data['name'])out['location'] = data['name'].toString() + ', ' + data['sys']['counntry'].toString()
+					//console.log(data)
+					out['image_url'] = 'http://openweathermap.org/img/w/' + data['weather'][0]['icon'] + '.png'
+					if(data['sys']['country']){
+						out['title'] = data['name'] + ', ' + data['sys']['country']
+					}else{
+						out['title'] = data['name']
+					}
 					out['main'] = data['weather'][0]['main']
 					out['temp'] = data['main']['temp']
 					out['pressure'] = data['main']['pressure']
 					out['humidity'] = data['main']['humidity']
-					out['temp_min'] = data['main']['temp_min']
-					out['temp_max'] = data['main']['temp_max']
 					out['visibility'] = data['visibility']
 					out['wind'] = data['wind']['speed']
 					out['clouds'] = data['clouds']['all']
@@ -59,7 +62,7 @@ const Weather = () => ({
 					out['location'] = data['city']['name'] + ', ' + data['city']['country']
 					data['list'].forEach( function(el, index) {
 						let obj = {}
-						obj['date'] = new Date(el['dt']*1000)
+						obj['date'] = new Date(el['dt']*1000).toDateString()
 						obj['pressure'] = el['pressure']
 						obj['humidity'] = el['humidity']
 						obj['wind'] = el['speed']
@@ -84,15 +87,15 @@ const Weather = () => ({
 
 module.exports = Weather
 
-/*
-var more= {'lat':'28.612205', 'lon':'77.034980'}
 
+var more= {'lat':'28.612205', 'lon':'77.034980'}
+/*
 Weather().forecast(more, (err, data) => {
 	if(!err && data){
 		console.log(data)
 	}
 })
-
+*/
 Weather().currentWeather(more, (err, data) => {
 	if(!err && data){
 		console.log(data)
@@ -101,4 +104,3 @@ Weather().currentWeather(more, (err, data) => {
 	}
 });
 
-*/
